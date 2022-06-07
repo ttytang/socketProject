c1c2ba0287716edf5d4044ba20e535cda7281f5c
don <267282100@qq.com>
2022-06-07 15:28:05 +0800

    add starter mechanism to preclude port scan behaviour

--- forwardserver.py
+++ forwardserver.py
@@ -1,12 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 -u
 import os.path
 import socket
 import threading
 import senderclient
 import receiverclient
 import struct
+import time
 
 class ReceiverServer(receiverclient.ReceiverClient):
+
+    def __init__(self, starter=b'\x20\x22\x06\x05', tailer=b'\xff\xff\xff\xff'):
+        super(ReceiverServer, self).__init__(starter=starter, tailer=tailer)
+        self.data_sock = None
+        self.data_ip = None
+        
+    def reset(self):
+        super(ReceiverServer, self).reset()
+        self.data_sock = None
+        self.data_ip = None
         
     def binding(self, receiver_addr=('127.0.0.1', 9997)):
         self.sock.bind(receiver_addr)
@@ -14,10 +25,35 @@ class ReceiverServer(receiverclient.ReceiverClient):
     def listening(self, link_num = 5):
         self.sock.listen(link_num)
 
+    ###def connection_ready(self):
+    ###    sock, addr = self.sock.accept()
+    ###    print('One sender is connecting from %s:%s...' % addr)
+    ###    time.sleep(1)
+    ###    try:
+    ###       starter = sock.recv(len(self.starter), socket.MSG_DONTWAIT)
+    ###       if (starter != self.starter):
+    ###           return sock, None
+    ###    except BlockingIOError:
+    ###        return sock, None
+    ###    print('Accept a new sender connection from %s:%s...' % addr)
+    ###    return sock, addr
+        
     def connection_ready(self):
-        sock, addr = self.sock.accept()
-        print('Accept new connection from %s:%s...' % addr)
-        return sock, addr
+        while(not self.data_ip):
+            sock, addr = self.sock.accept()
+            print('One sender is connecting from %s:%s...' % addr)
+            time.sleep(1)
+            try:
+                starter = sock.recv(len(self.starter), socket.MSG_DONTWAIT)
+                if (starter != self.starter):
+                    self.close_session(sock)
+                    continue
+            except BlockingIOError:
+                self.close_session(sock)
+                continue
+            print('Accept a new sender connection from %s:%s...' % addr)
+            self.data_sock = sock
+            self.data_ip = addr
 
     def send_warm_message(self, sock, addr, warm_message = b'Welcome'):
         print('Welcome %s:%s...' % addr)
@@ -51,16 +87,37 @@ class ReceiverServer(receiverclient.ReceiverClient):
 ###
 class SenderServer(senderclient.SenderClient):
 
+    def __init__(self, starter=b'\x20\x22\x06\x05', tailer=b'\xff\xff\xff\xff'):
+        super(SenderServer, self).__init__(starter=starter, tailer=tailer)
+        self.data_sock = None
+        self.data_ip = None
+        
+    def reset(self):
+        self.data_sock = None
+        self.data_ip = None        
+
     def binding(self, sender_addr=('127.0.0.1', 9998)):
         self.sock.bind(sender_addr)
 
     def listening(self, link_num=5):
         self.sock.listen(link_num)
-
+    
     def connection_ready(self):
-        sock, addr = self.sock.accept()
-        print('Accept new connection from %s:%s...' % addr)
-        return sock, addr
+        while(not self.data_ip):
+            sock, addr = self.sock.accept()
+            print('One receiver is connecting from %s:%s...' % addr)
+            time.sleep(1)
+            try:
+                starter = sock.recv(len(self.starter), socket.MSG_DONTWAIT)
+                if (starter != self.starter):
+                    self.close_session(sock)
+                    continue
+            except BlockingIOError:
+                self.close_session(sock)
+                continue
+            print('Accept a new receiver connection from %s:%s...' % addr)
+            self.data_sock = sock
+            self.data_ip = addr
 
     def send_warm_message(self, sock, addr, warm_message=b'Welcome'):
         print('Welcome %s:%s...' % addr)
@@ -76,10 +133,37 @@ if __name__ == "__main__":
     sender = SenderServer()
     sender.binding(('0.0.0.0',9998))
     sender.listening()
+    ###while True:
+    ###    sender_sock, destin_ip = sender.connection_ready()
+    ###    if (not destin_ip):
+    ###        sender.close_session(sender_sock)
+    ###        continue# means destionation receiver needs to open for the first
+    ###    #create a thread to receive stream
+    ###    receiver_sock, source_ip = receiver.connection_ready()
+    ###    if (not source_ip):
+    ###        receiver.close_session(receiver_sock)
+    ###        continue
+    ###    t = threading.Thread(target=receiver.link_handle, args=(receiver_sock, source_ip, b'Welcome'))
+    ###    t.start()
+    ###    #while(receiver.stream_buffer or not receiver.stream_num):
+    ###    while(receiver.stream_buffer or not receiver.tailer_flag):
+    ###        if (receiver.stream_buffer):
+    ###            sender.send_msg(sender_sock, receiver.stream_buffer.pop(0))
+    ###    sender.send_tailer(sender_sock)
+    ###    sender.close_session(sender_sock)
+    ###    receiver.reset()
+    ###    print('session write complete!(9')
+        
     while True:
+        s = threading.Thread(target=sender.connection_ready)
+        s.start()
         #create a thread to receive stream
-        receiver_sock, source_ip = receiver.connection_ready()
-        sender_sock, destin_ip = sender.connection_ready()
+        receiver.connection_ready()
+        receiver_sock = receiver.data_sock
+        source_ip = receiver.data_ip
+        s.join()#wait destination receiver ready
+        sender_sock = sender.data_sock
+        destin_ip = sender.data_ip
         t = threading.Thread(target=receiver.link_handle, args=(receiver_sock, source_ip, b'Welcome'))
         t.start()
         #while(receiver.stream_buffer or not receiver.stream_num):
@@ -89,7 +173,8 @@ if __name__ == "__main__":
         sender.send_tailer(sender_sock)
         sender.close_session(sender_sock)
         receiver.reset()
-        print('session write complete!(8')
+        sender.reset()
+        print('session write complete!(9')
 
 ###used only the receiver
 ###if __name__ == "__main__":
--- senderclient.py
+++ senderclient.py
@@ -4,8 +4,10 @@ import sys
 
 
 class SenderClient:
-    def __init__(self):
+    def __init__(self, starter=b'\x20\x22\x06\x05', tailer=b'\xff\xff\xff\xff'):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.starter = starter
+        self.tailer = tailer
 
     def connect_server(self, ipaddr='127.0.0.1', port=9997):
         self.sock.connect((ipaddr, port))
@@ -15,8 +17,11 @@ class SenderClient:
         msg = struct.pack('>I', len(msg)) + msg
         sock.send(msg)
 
-    def send_tailer(self, sock, tailer=b'\xff\xff\xff\xff'):
-        sock.send(tailer)
+    def send_starter(self, sock):
+        sock.send(self.starter)
+
+    def send_tailer(self, sock):
+        sock.send(self.tailer)
 
     def close_session(self, sock):
         sock.close()
@@ -26,6 +31,7 @@ if __name__ == "__main__":
     sender = SenderClient()
     sender.connect_server('47.108.64.28', 9997)
     #sender.connect_server()
+    sender.send_starter(sender.sock)
     print(sender.sock.recv(1024).decode('utf-8'))
     fpath = sys.argv[1]
     print(fpath)
